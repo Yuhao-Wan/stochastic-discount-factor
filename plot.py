4a18fe252e29198328deaa44cea25fd1dfeb9779
Yuhao-Wan <yuhao.diane.wan@gmail.com>
2019-05-06 23:36:08 -0700

    add plot for new results

--- plot.py
+++ plot.py
@@ -3,9 +3,14 @@ import numpy as np
 from baselines.common import plot_util as pu
 from baselines import logger
 
-
 #print(logger.get_dir())
-results = pu.load_results('~/Desktop/Scholars/stochastic-discount-factor/logs/')
-r = results[0]
-plt.plot(r.progress.steps, r.progress["mean 100 episode reward"])
-plt.show()
+results = pu.load_results('~/Desktop/Scholars/stochastic-discount-factor/logs/exp1')
+names = []
+for i in range(11):
+    r = results[i]
+    name = r.dirname.split('/')[-1]
+    plt.plot(np.divide(r.progress.steps, 1000), r.progress["mean 100 episode reward"])
+    names.append(name)
+
+plt.legend(names)
+plt.savefig('./logs/exp1/plot3.png')
