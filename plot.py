59ac95f88df056935adf2688e7f8eccba249f32f
Yuhao-Wan <yuhao.diane.wan@gmail.com>
2019-05-08 12:13:58 -0700

    add various plots

--- plot.py
+++ plot.py
@@ -4,13 +4,15 @@ from baselines.common import plot_util as pu
 from baselines import logger
 
 #print(logger.get_dir())
-results = pu.load_results('~/Desktop/Scholars/stochastic-discount-factor/logs/exp1')
+results = pu.load_results('~/Desktop/Scholars/stochastic-discount-factor/logs/simple/explore05/')
 names = []
-for i in range(11):
+for i in range(3):
     r = results[i]
-    name = r.dirname.split('/')[-1]
+    name = r.dirname.split('/')[-2]
     plt.plot(np.divide(r.progress.steps, 1000), r.progress["mean 100 episode reward"])
+    #_, right = plt.xlim()
+    #plt.xlim(right=1500)  
     names.append(name)
 
 plt.legend(names)
-plt.savefig('./logs/exp1/plot3.png')
+plt.savefig('./logs/simple/explore05/plot.png')
