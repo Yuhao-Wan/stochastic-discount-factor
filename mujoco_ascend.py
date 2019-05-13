74431143ce4082982bb0aec4fff448073137aeb9
Yuhao-Wan <yuhao.diane.wan@gmail.com>
2019-05-12 20:51:00 -0700

    ensure same params as baselines

--- mujoco_ascend.py
+++ mujoco_ascend.py
@@ -12,90 +12,48 @@ import ppo2
 dirs = './logs/mujoco/ascend/'
 
 
-# def main(gpu, seed, lam_fraction, final_lam, path):
-#     with tf.device('/device:GPU:%s' % gpu):
+def main(gpu, envid, seed, lam_fraction, final_lam, path):
+    with tf.device('/device:GPU:%s' % gpu):
 
-#         logger.configure(dir=dirs+'%s/%s/' % (path, seed))
+        logger.configure(dir=dirs+'%s/%s/' % (path, seed))
 
-#         env = gym.make("HalfCheetah-v2")
-#         env = bench.Monitor(env, logger.get_dir())
-#         env = DummyVecEnv([lambda: env]) 
+        env = gym.make(envid)
+        env = bench.Monitor(env, logger.get_dir())
+        env = DummyVecEnv([lambda: env]) 
 
-#         kwargs = dict(network='mlp', 
-#                       env=env, 
-#                       total_timesteps=500000, 
-#                       seed=seed,
-#                       nsteps=2048,
-#                       nminibatches=32,
-#                       lam=0.1, 
-#                       lam_fraction=lam_fraction, 
-#                       final_lam=final_lam,
-#                       gamma=0.99,
-#                       noptepochs=10,
-#                       log_interval=1,
-#                       ent_coef=0.0,
-#                       lr=lambda f: 3e-4 * f,
-#                       cliprange=0.2,
-#                       value_network='copy')
+        kwargs = dict(network='mlp', 
+                      env=env, 
+                      total_timesteps=1000000, 
+                      seed=seed,
+                      nsteps=2048,
+                      nminibatches=32,
+                      lam=0.1, 
+                      lam_fraction=lam_fraction, 
+                      final_lam=final_lam,
+                      gamma=0.99,
+                      noptepochs=10,
+                      log_interval=1,
+                      ent_coef=0.0,
+                      lr=lambda f: 3e-4 * f,
+                      cliprange=0.2,
+                      value_network='copy')
 
-#         f = open(dirs+'%s/%s/params.txt' % (path, seed), 'w')
-#         f.write(str(kwargs))
-#         f.close()
+        f = open(dirs+'%s/%s/params.txt' % (path, seed), 'w')
+        f.write(str(kwargs))
+        f.close()
 
-#         model = ppo2.learn(**kwargs)
-#         model.save(dirs+'%s/%s/model.pkl' % (path, seed))
+        model = ppo2.learn(**kwargs)
+        model.save(dirs+'%s/%s/model.pkl' % (path, seed))
 
-#         env.close()
-
-
-# if __name__ == '__main__':
-#     gpu = int(sys.argv[1])
-#     seed = int(sys.argv[2])
-#     lam_fraction = float(sys.argv[3])
-#     final_lam = float(sys.argv[4])
-#     path = sys.argv[5]
-
-#     main(gpu, seed, lam_fraction, final_lam, path)
-
-def main(seed, lam_fraction, final_lam, path):
-
-    logger.configure(dir=dirs+'%s/%s/' % (path, seed))
-
-    env = gym.make("HalfCheetah-v2")
-    env = bench.Monitor(env, logger.get_dir())
-    env = DummyVecEnv([lambda: env]) 
-
-    kwargs = dict(network='mlp', 
-                env=env, 
-                total_timesteps=500000, 
-                seed=seed,
-                nsteps=2048,
-                nminibatches=32,
-                lam=0.1, 
-                lam_fraction=lam_fraction, 
-                final_lam=final_lam,
-                gamma=0.99,
-                noptepochs=10,
-                log_interval=1,
-                ent_coef=0.0,
-                lr=lambda f: 3e-4 * f,
-                cliprange=0.2,
-                value_network='copy')
-
-    f = open(dirs+'%s/%s/params.txt' % (path, seed), 'w')
-    f.write(str(kwargs))
-    f.close()
-
-    model = ppo2.learn(**kwargs)
-    model.save(dirs+'%s/%s/model.pkl' % (path, seed))
-
-    env.close()
+        env.close()
 
 
 if __name__ == '__main__':
-    seed = int(sys.argv[1])
-    lam_fraction = float(sys.argv[2])
-    final_lam = float(sys.argv[3])
-    path = sys.argv[4]
+    gpu = int(sys.argv[1])
+    envid = sys.argv[2]
+    seed = int(sys.argv[3])
+    lam_fraction = float(sys.argv[4])
+    final_lam = float(sys.argv[5])
+    path = sys.argv[6]
 
-    main(seed, lam_fraction, final_lam, path)
+    main(gpu, envid, seed, lam_fraction, final_lam, path)
