26f94fbab96f7bc34854ba9f86a2dda70acd090d
Yuhao-Wan <yuhao.diane.wan@gmail.com>
2019-05-21 10:14:06 +0800

    fix import errors

--- experiments/train_dense.py
+++ experiments/train_dense.py
@@ -12,41 +12,41 @@ from baselines.common import plot_util as pu
 
 
 
-dirs = './logs/maze1/'
+dirs = '../logs/'
 
 def main(seed, fraction, discount, path, gpu):
     with tf.device('/device:GPU:%s' % gpu):
       
         logger.configure(dir=dirs+'%s/%s/' % (path, seed), format_strs=['csv'])
 
-        kwargs = dict(seed=seed,
-            network=models.mlp(num_layers=2, num_hidden=128, activation=tf.nn.relu),
-            lr=1e-4,
-            total_timesteps=1500000,
-            buffer_size=150000,
-            #exploration_fraction=1.0, #random act
-            #exploration_final_eps=1.0, #random act
-            exploration_fraction=0.2,
-            exploration_final_eps=0.02, 
-            learning_starts=2000,
-            target_network_update_freq=500,
-            myopic_fraction=fraction,
-            final_gamma=discount,
-            gamma=discount,
-            prioritized_replay=True,
-            prioritized_replay_alpha=0.6,
-            print_freq=5)
-
+        kwargs = dict(
+                    seed=seed,
+                    network=models.mlp(num_layers=2, num_hidden=128, activation=tf.nn.relu),
+                    lr=1e-4,
+                    total_timesteps=1500000,
+                    buffer_size=150000,
+                    #exploration_fraction=1.0, #random act
+                    #exploration_final_eps=1.0, #random act
+                    exploration_fraction=0.2,
+                    exploration_final_eps=0.02, 
+                    learning_starts=2000,
+                    target_network_update_freq=500,
+                    myopic_fraction=fraction,
+                    final_gamma=discount,
+                    #gamma=discount,
+                    prioritized_replay=True,
+                    prioritized_replay_alpha=0.6,
+                    print_freq=5)
 
         f = open(dirs+'%s/%s/params.txt' % (path, seed), 'w')
         f.write(str(kwargs))
         f.close()
 
-        env = gym.make("Dense-v0")
+        env = gym.make("dense-v0")
         act = deepq.learn(
             env=env,
             **kwargs)
-                      
+                        
         print("Saving model to maze.pkl")
 
         act.save(dirs+'%s/%s/maze.pkl' % (path, seed))
@@ -66,4 +66,5 @@ if __name__ == '__main__':
     discount = float(sys.argv[3])
     path = sys.argv[4]
     gpu = sys.argv[5]
+    
     main(seed, fraction, discount, path, gpu)
